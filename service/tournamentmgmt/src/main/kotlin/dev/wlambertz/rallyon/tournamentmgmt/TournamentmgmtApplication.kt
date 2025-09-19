af9b5c09867cd72567a2512ebfc1f88fbdf5cab9
wlambertz <wolfgang.lambertz@protonmail.com>
2025-09-19 10:39:50 +0200

    chore: update .gitattributes to ensure gradlew maintains LF line endings; remove empty .keep file from tournamentmgmt directory

---
+++ service/tournamentmgmt/src/main/kotlin/dev/wlambertz/rallyon/tournamentmgmt/TournamentmgmtApplication.kt
@@ -0,0 +1,11 @@
+package dev.wlambertz.rallyon.tournamentmgmt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TournamentmgmtApplication
+
+fun main(args: Array<String>) {
+	runApplication<TournamentmgmtApplication>(*args)
+}
