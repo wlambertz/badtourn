147d05f3b87d40d8f4346e6736dac921f90bf8af
wlambertz <wolfgang.lambertz@protonmail.com>
2025-10-16 15:22:17 +0200

    chore: remove outdated TODO comment from Court class for clarity

--- service/tournamentmgmt/src/main/java/dev/wlambertz/rallyon/tournamentmgmt/setup/configuration/api/Court.java
+++ service/tournamentmgmt/src/main/java/dev/wlambertz/rallyon/tournamentmgmt/setup/configuration/api/Court.java
@@ -1,5 +1,4 @@
 package dev.wlambertz.rallyon.tournamentmgmt.setup.configuration.api;
-// TODO(review): Confirm court identity strategy and attributes
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
